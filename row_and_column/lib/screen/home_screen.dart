3cec6583cfe52a505561815c21339a9975992679
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-23 22:34:56 +0900

    Row MainAxisAlignment

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -10,8 +10,8 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Container(
           color: Colors.black,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: colors
                 .map(
                   (e) => Container(
