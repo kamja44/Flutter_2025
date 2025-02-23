4580147d944dd2c2bfc83a08ad130cd7c349a759
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-23 11:45:49 +0900

    StatelessWidget 선언해보기

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF335CB0),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('asset/img/logo.png'),
-            const CircularProgressIndicator(
-              color: Colors.white,
-            )
-          ],
-        ),
-      ),
+    const MaterialApp(
+      home: HomeScreen(),
     ),
   );
+}
+
+// StatelessWidget
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF335CB0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('asset/img/logo.png'),
+          const CircularProgressIndicator(
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
 }
