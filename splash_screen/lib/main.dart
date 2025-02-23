5ec4027355846440de9a0dbad8cfde558cdeab1c
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-23 11:33:01 +0900

    Image 위젯 사용하기

---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Image.asset('asset/img/logo.png'),
+      ),
+    ),
+  );
+}
