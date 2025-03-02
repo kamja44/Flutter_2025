76d4873633efd91f6085dc6c7833de06543fa1f6
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-02 12:34:57 +0900

    EdgeInsets.fromLTRB 생성자

--- padding_theory/lib/screen/home_screen.dart
+++ padding_theory/lib/screen/home_screen.dart
@@ -10,11 +10,11 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           color: Colors.red,
           child: Padding(
-            padding: const EdgeInsets.only(
-              left: 4.0,
-              right: 8.0,
-              top: 12.0,
-              bottom: 16.0,
+            padding: const EdgeInsets.fromLTRB(
+              32.0,
+              64.0,
+              16.0,
+              8.0,
             ),
             child: Container(
               color: Colors.blue,
