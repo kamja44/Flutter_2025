8d0205cb181eb1329b0063e6eaab7f6fddab70f8
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-03 23:15:09 +0900

    기본 레이아웃 작업

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -5,6 +5,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 제목과 아이콘
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('제목'),
+                IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+              ],
+            ),
+            // 숫자
+            Expanded(
+              child: Text('''
+              123
+              456
+              789
+              '''),
+            ),
+            // 버튼
+            ElevatedButton(onPressed: () {}, child: Text('생성하기')),
+          ],
+        ),
+      ),
+    );
   }
 }
