195d0341b1f7fd2b270cb74803994741f0b690ea
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-03 12:59:08 +0900

    Image Carousel

--- image_carousel/lib/screen/home_sreen.dart
+++ image_carousel/lib/screen/home_sreen.dart
@@ -1,12 +1,53 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomeSreen extends StatelessWidget {
+class HomeSreen extends StatefulWidget {
   const HomeSreen({super.key});
+
+  @override
+  State<HomeSreen> createState() => _HomeSreenState();
+}
+
+class _HomeSreenState extends State<HomeSreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controller,
         children:
             [1, 2, 3, 4, 5]
                 .map(
