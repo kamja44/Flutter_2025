2616a8ac42c54cbf75969acd8d1af3e7bd06143d
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-03 16:17:35 +0900

    상태 상위로 올리기

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +21,74 @@ class HomeScreen extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              _Top(),
+              _Top(selectedDate: selectedDate, onPressed: onHeartPressed),
               //이미지
               _Bottom(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
+              dateOrder: DatePickerDateOrder.ymd,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _Top extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback? onPressed;
+
+  const _Top({super.key, required this.selectedDate, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final textTheme = Theme.of(context).textTheme;
+
+    return Expanded(
+      flex: 1,
+      child: Column(
+        children: [
+          Text('U & I', style: textTheme.displayLarge),
+          Text('우리 처음 만난 날', style: textTheme.bodyLarge),
+          Text(
+            '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+            style: textTheme.bodyMedium,
+          ),
+          IconButton(
+            iconSize: 60,
+            color: Colors.red,
+            onPressed: onPressed,
+            icon: Icon(Icons.favorite),
+          ),
+          Text(
+            'D+${now.difference(selectedDate).inDays + 1}',
+            style: textTheme.displayMedium,
+          ),
+        ],
       ),
     );
   }
@@ -33,72 +102,6 @@ class _Bottom extends StatelessWidget {
     return Expanded(
       flex: 1,
       child: Container(child: Image.asset('asset/img/middle_image.png')),
-    );
-  }
-}
-
-class _Top extends StatefulWidget {
-  const _Top({super.key});
-
-  @override
-  State<_Top> createState() => _TopState();
-}
-
-class _TopState extends State<_Top> {
-  DateTime selectedDate = DateTime.now();
-
-  @override
-  Widget build(BuildContext context) {
-    final now = DateTime.now();
-    final TextTheme = Theme.of(context).textTheme;
-
-    return Expanded(
-      flex: 1,
-      child: Container(
-        child: Column(
-          children: [
-            Text('U & I', style: TextTheme.displayLarge),
-            Text('우리 처음 만난 날', style: TextTheme.bodyLarge),
-            Text(
-              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-              style: TextTheme.bodyMedium,
-            ),
-            IconButton(
-              iconSize: 60,
-              color: Colors.red,
-              onPressed: () {
-                showCupertinoDialog(
-                  context: context,
-                  barrierDismissible: true,
-                  builder: (BuildContext context) {
-                    return Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        color: Colors.white,
-                        height: 300,
-                        child: CupertinoDatePicker(
-                          mode: CupertinoDatePickerMode.date,
-                          onDateTimeChanged: (DateTime date) {
-                            setState(() {
-                              selectedDate = date;
-                            });
-                          },
-                          dateOrder: DatePickerDateOrder.ymd,
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-              icon: Icon(Icons.favorite),
-            ),
-            Text(
-              'D+${now.difference(selectedDate).inDays + 1}',
-              style: TextTheme.displayMedium,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
