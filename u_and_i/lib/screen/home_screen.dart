65cf866cc7ef65f6ebe3682d02a5b24282708806
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-03 14:44:06 +0900

    테마 적용해보기

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -13,62 +13,52 @@ class HomeScreen extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  child: Column(
-                    children: [
-                      Text(
-                        'U & I',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'parisienne',
-                          fontSize: 80,
-                        ),
-                      ),
-                      Text(
-                        '우리 처음 만난 날',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontFamily: 'sunflower',
-                        ),
-                      ),
-                      Text(
-                        '2025.03.03',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontSize: 20,
-                        ),
-                      ),
-                      IconButton(
-                        iconSize: 60,
-                        color: Colors.red,
-                        onPressed: () {},
-                        icon: Icon(Icons.favorite),
-                      ),
-                      Text(
-                        'D+1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 50,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  child: Image.asset('asset/img/middle_image.png'),
-                ),
-              ),
+              _Top(),
+              //이미지
+              _Bottom(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(child: Image.asset('asset/img/middle_image.png')),
+    );
+  }
+}
+
+class _Top extends StatelessWidget {
+  const _Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme = Theme.of(context).textTheme;
+
+    return Expanded(
+      flex: 1,
+      child: Container(
+        child: Column(
+          children: [
+            Text('U & I', style: TextTheme.displayLarge),
+            Text('우리 처음 만난 날', style: TextTheme.bodyLarge),
+            Text('2025.03.03', style: TextTheme.bodyMedium),
+            IconButton(
+              iconSize: 60,
+              color: Colors.red,
+              onPressed: () {},
+              icon: Icon(Icons.favorite),
+            ),
+            Text('D+1', style: TextTheme.displayMedium),
+          ],
         ),
       ),
     );
