54a67d839fc270de4981deaaee64c12edd14fa09
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-03 15:46:04 +0900

    DatePicker 사용해보기

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -54,7 +55,28 @@ class _Top extends StatelessWidget {
             IconButton(
               iconSize: 60,
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  builder: (BuildContext context) {
+                    return Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        color: Colors.white,
+                        height: 300,
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.date,
+                          onDateTimeChanged: (DateTime date) {
+                            print(date);
+                          },
+                          dateOrder: DatePickerDateOrder.ymd,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
               icon: Icon(Icons.favorite),
             ),
             Text('D+1', style: TextTheme.displayMedium),
