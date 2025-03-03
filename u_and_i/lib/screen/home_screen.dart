286c88688f546ec7f8406ff5b36fa0b8ab132a1e
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-03 14:01:21 +0900

    U & I application 레이아웃 작업하기 with MediaQuery, 디자인 마무리하기

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -5,6 +5,72 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        'U & I',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'parisienne',
+                          fontSize: 80,
+                        ),
+                      ),
+                      Text(
+                        '우리 처음 만난 날',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontFamily: 'sunflower',
+                        ),
+                      ),
+                      Text(
+                        '2025.03.03',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'sunflower',
+                          fontSize: 20,
+                        ),
+                      ),
+                      IconButton(
+                        iconSize: 60,
+                        color: Colors.red,
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite),
+                      ),
+                      Text(
+                        'D+1',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'sunflower',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Image.asset('asset/img/middle_image.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
