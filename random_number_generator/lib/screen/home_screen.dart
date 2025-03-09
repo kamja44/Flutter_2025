daafe9a7638b4b61af2b3865e9ca59f8af6197de
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-03-09 16:00:43 +0900

    랜덤숫자 map() 함수 사용해서 매핑하기

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -50,11 +50,29 @@ class _Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Text('''
-                123
-                456
-                789
-                '''),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:
+            [
+                  [1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9],
+                ]
+                .map(
+                  (item) => Row(
+                    children:
+                        item
+                            .map(
+                              (number) => Text(
+                                number.toString(),
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            )
+                            .toList(),
+                  ),
+                )
+                .toList(),
+      ),
     );
   }
 }
