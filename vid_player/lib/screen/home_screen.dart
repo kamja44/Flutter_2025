57b41fecc72407d0e80a6fbd75ce1a58f8cb9f7f
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-08-26 04:45:10 +0900

    VideoPlayer 141 위젯 스위칭 with setStatus

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showVideoPlayer = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF2A3A7C), Color(0xFF000118)],
-          ),
+      body:
+          showVideoPlayer
+              ? _VideoPlayer()
+              : _VideoSelector(onLogoTap: onLogoTap),
+    );
+  }
+
+  onLogoTap() {
+    setState(() {
+      showVideoPlayer = true;
+    });
+  }
+}
+
+class _VideoSelector extends StatelessWidget {
+  final VoidCallback onLogoTap;
+  const _VideoSelector({super.key, required this.onLogoTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF2A3A7C), Color(0xFF000118)],
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [_Logo(), SizedBox(height: 20.0), _Title()],
-        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [_Logo(onTap: onLogoTap), SizedBox(height: 20.0), _Title()],
       ),
     );
   }
@@ -45,10 +71,24 @@ class _Title extends StatelessWidget {
 }
 
 class _Logo extends StatelessWidget {
-  const _Logo({super.key});
+  final VoidCallback onTap;
+
+  const _Logo({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset('asset/image/logo.png');
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset('asset/image/logo.png'),
+    );
+  }
+}
+
+class _VideoPlayer extends StatelessWidget {
+  const _VideoPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Video'));
   }
 }
